9f8d8602e9ad2edfe225964e871aaca5f4960404
rayan-albouni <rayanbouni@gmail.com>
2025-08-29 22:58:56 +0200

    Fixing dependencies issue

--- src/DocumentClassificationService.Functions/Program.cs
+++ src/DocumentClassificationService.Functions/Program.cs
@@ -3,12 +3,12 @@ using DocumentClassificationService.Infrastructure;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = FunctionsApplication.CreateBuilder(args);
-
 builder.ConfigureFunctionsWebApplication();
-builder.Services.AddFunctionsWorkerDefaults();
 builder.Services.AddApplicationInsightsTelemetryWorkerService();
+builder.Logging.AddApplicationInsights();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
